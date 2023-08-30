63d725e20efa8392bc5ddcf4b76b85610f75063e
Vivian <vivianhuang197@gmail.com>
2023-08-30 21:42:23 +0800

    新增首頁樣式

---
+++ main.js
@@ -0,0 +1,19 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Swiper from 'swiper';
+
+const swiper = new Swiper(".commentSwiper",{
+    spaceBetween: 24,
+  // 滑鼠中鍵滾輪可滑動
+  mousewheel: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true
+  },
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+
+});
