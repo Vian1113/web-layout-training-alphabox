6fa92630a3477e3284867d27a2817d2ed013e7a4
Vivian <vivianhuang197@gmail.com>
2023-08-30 22:59:20 +0800

    新增「使用分享」區塊

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Swiper from 'swiper';
 
+
 const swiper = new Swiper(".commentSwiper",{
     spaceBetween: 24,
   // 滑鼠中鍵滾輪可滑動
@@ -14,6 +15,8 @@ const swiper = new Swiper(".commentSwiper",{
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
-  }
+  },
+
+
 
 });
