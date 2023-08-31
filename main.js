c56e856d3cc1734a1083b2f8a4d9fbb26bf8ec1e
Vivian <vivianhuang197@gmail.com>
2023-08-31 23:40:16 +0800

    新增產品頁面

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 
-
+//index swiper
 const swiper = new Swiper(".commentSwiper",{
-    spaceBetween: 24,
+  spaceBetween: 24,
   // 滑鼠中鍵滾輪可滑動
   mousewheel: true,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true
-  },
+  direction: 'vertical',
+  
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  //   draggable: true
+  // },
   slidesPerView: "auto",
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   },
+  breakpoints:{
+    992:{
+      direction: 'horizontal',
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true
+      },
+    }
+    
+  },
 
 
+
+});
+
+//product swiper
+const swiperCharact = new Swiper(".charactSwiper",{
+  // autoplay: {
+  //   delay: 800,
+  // },
+  
+  loop: true,
+  slidesPerView: "auto",
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  
 
 });
