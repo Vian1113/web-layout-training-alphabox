50e983464063feceb6347dd34d63e027ed18367d
Vivian <vivianhuang197@gmail.com>
2023-09-01 22:09:31 +0800

    完成產品資訊頁面

--- main.js
+++ main.js
@@ -2,8 +2,35 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Swiper from 'swiper/bundle';
 
-//index swiper
-const swiper = new Swiper(".commentSwiper",{
+//index intro swiper
+const swiperIntro = new Swiper(".introSwiper",{
+  loop: true,
+  speed: 1000,
+   autoplay: {
+     delay: 3000,
+     disableOnInteraction: false,
+   },
+  
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints:{
+    768: {
+      autoplay: {
+          enabled: true, //設定為false 為768px 以上時，放棄所有功能；設定為true 為768px 以上時，可調整下列設定
+          delay: 1000,
+        }
+    }
+    
+  },
+
+
+
+});
+
+//index comment swiper
+const swiperComment = new Swiper(".commentSwiper",{
   spaceBetween: 24,
   // 滑鼠中鍵滾輪可滑動
   mousewheel: true,
@@ -31,10 +58,7 @@ const swiper = new Swiper(".commentSwiper",{
 
 //product swiper
 const swiperCharact = new Swiper(".charactSwiper",{
-  // autoplay: {
-  //   delay: 800,
-  // },
-  
+
   loop: true,
   slidesPerView: "auto",
 
